9c8706263cdccca45ffd75577499cd4b73c13277
ShockDexterity <canterw8353@my.uwstout.edu>
2021-11-04 11:05:01 -0500

    added health stuff

---
+++ Sprint 4/Assets/Scripts/PlayerScripts/HealthController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HealthController : MonoBehaviour
+{
+    public RectTransform rectTransform;
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] healthSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform.localScale = new Vector3(70f, 70f, 0);
+        healthSprites = Resources.LoadAll<Sprite>("");
+
+        // healthpng = this.GetComponent<Image>();
+        this.GetComponent<Image>().image = healthSprites[16].texture;
+
+        //spriteRenderer.sprite = healthSprites[16];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
